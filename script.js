d27ab89ea8a5da9efbd33d9c378e839e6970bab0
Confidence Madiope <confidencem97@gmail.com>
2022-05-10 18:49:20 +0200

    appended the divs to the msin container div

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let container = document.querySelector(".container");
+const container = document.querySelector(".container");
 let div1 = document.createElement("div");
 div1.classList.add("div1");
 let div2 = document.createElement("div");
@@ -11,7 +11,7 @@ let div5 = document.createElement("div");
 div5.classList.add("div5");
 let div6 = document.createElement("div");
 div6.classList.add("div");
-let div7 = document.creteElement("div");
+let div7 = document.createElement("div");
 div7.classList.add("div");
 let div8 = document.createElement("div");
 div8.classList.add("div");
@@ -32,4 +32,35 @@ div15.classList.add("div");
 let div16 = document.createElement("div");
 div16.classList.add("div");
 
-container.appendChild(document.querySelectorAll('.div'))
+document.getElementById('container').appendChild(div1);
+document.getElementById("container").appendChild(div2);
+document.getElementById("container").appendChild(div3);
+document.getElementById("container").appendChild(div4);
+document.getElementById("container").appendChild(div5);
+document.getElementById("container").appendChild(div6);
+document.getElementById("container").appendChild(div7);
+document.getElementById("container").appendChild(div8);
+document.getElementById("container").appendChild(div9);
+document.getElementById("container").appendChild(div10);
+document.getElementById("container").appendChild(div11);
+document.getElementById("container").appendChild(div12);
+document.getElementById("container").appendChild(div13);
+document.getElementById("container").appendChild(div14);
+document.getElementById("container").appendChild(div15);
+document.getElementById("container").appendChild(div16);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
